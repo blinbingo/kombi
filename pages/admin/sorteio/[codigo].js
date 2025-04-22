8e07386c64a8d36a91cff9b495b52e0ffc2abec3
blinbingo <ian.oddssports@gmail.com>
2025-04-22 16:33:33 -0300

    Update [codigo].js

--- pages/admin/sorteio/[codigo].js
+++ pages/admin/sorteio/[codigo].js
@@ -210,28 +210,12 @@ export default function SorteioManual() {
 
       {etapasAlcancadas.includes(100) && !encerrado && (
         <button
-         onClick={async () => {
-  console.log("Código:", codigo);
-  console.log("🔢 bolas:", bolasSelecionadas);
-  console.log("✅ convertidas:", bolasSelecionadas.map(Number));
-
-  const { error } = await supabase
-    .from("bingo")
-    .upsert({
-      codigoSorteio: codigo,
-      bolas: bolasSelecionadas.map(Number),
-      premiados: {
-        25: Object.fromEntries((premios[25] || []).map((c) => [c, 10])),
-        50: Object.fromEntries((premios[50] || []).map((c) => [c, 20])),
-        75: Object.fromEntries((premios[75] || []).map((c) => [c, 200])),
-        100: Object.fromEntries((premios[100] || []).map((c) => [c, 500]))
-      },
-      resumo: resumoFinanceiro,
-      encerradoEm: new Date().toISOString()
-    }, { onConflict: ['codigoSorteio'] });
-
+          onClick={async () => {
+            console.log("Código:", codigo);
+            const { error } = await supabase
               .from("bingo")
-              .update({
+              .upsert({
+                codigoSorteio: codigo,
                 bolas: bolasSelecionadas,
                 premiados: {
                   25: Object.fromEntries((premios[25] || []).map((c) => [c, 10])),

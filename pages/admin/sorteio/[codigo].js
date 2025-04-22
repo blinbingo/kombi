9a2936df8da2a6f2d98d99f69d562b0cabec4e71
blinbingo <ian.oddssports@gmail.com>
2025-04-22 14:08:03 -0300

    Update [codigo].js

--- pages/admin/sorteio/[codigo].js
+++ pages/admin/sorteio/[codigo].js
@@ -215,7 +215,10 @@ export default function SorteioManual() {
             const { error } = await supabase
               .from("bingo")
               .update({
-                bolas: bolasSelecionadas.map(Number),
+                bolas: Array.isArray(bolasSelecionadas)
+  ? bolasSelecionadas.map((n) => parseInt(n, 10))
+  : [],
+
                 premiados: {
                   25: Object.fromEntries((premios[25] || []).map((c) => [c, 10])),
                   50: Object.fromEntries((premios[50] || []).map((c) => [c, 20])),

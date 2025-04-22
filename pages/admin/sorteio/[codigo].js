7c848c411c4b7ed2bb96d4ade3a0fbc9a59a9c36
blinbingo <ian.oddssports@gmail.com>
2025-04-22 11:57:11 -0300

    Update [codigo].js

--- pages/admin/sorteio/[codigo].js
+++ pages/admin/sorteio/[codigo].js
@@ -210,20 +210,16 @@ export default function SorteioManual() {
       {etapasAlcancadas.includes(100) && !encerrado && (
         <button
           onClick={async () => {
-            const { error } = await supabase
-              .from("bingo")
-              .update({
-                bolas: bolasSelecionadas,
-                premiados: {
-                  25: Object.fromEntries((premios[25] || []).map((c) => [c, 10])),
-                  50: Object.fromEntries((premios[50] || []).map((c) => [c, 20])),
-                  75: Object.fromEntries((premios[75] || []).map((c) => [c, 200])),
-                  100: Object.fromEntries((premios[100] || []).map((c) => [c, 500]))
-                },
-                resumo: resumoFinanceiro,
-                encerradoEm: new Date().toISOString()
-              })
-              .eq("codigoSorteio", codigo);
+          const { error } = await supabase
+  .from("bingo")
+  .update({
+    // ...
+  })
+  .eq("codigoSorteio", codigo);
+
+console.log("CÓDIGO DO SORTEIO:", codigo);
+console.log("ERRO:", error);
+
 
             if (error) {
               setMensagem("❌ Erro ao encerrar sorteio!");

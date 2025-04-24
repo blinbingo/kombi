caf7d161cff59d7d3bc86495b32aa96d74acc578
blinbingo <ian.oddssports@gmail.com>
2025-04-24 14:07:46 -0300

    Create getSorteioByCodigo.js

---
+++ utils/getSorteioByCodigo.js
@@ -0,0 +1,16 @@
+import { supabase } from '../client';
+
+export async function getSorteioByCodigo(codigo) {
+  const { data, error } = await supabase
+    .from('bingo')
+    .select('*')
+    .eq('codigo', codigo)
+    .single();
+
+  if (error) {
+    console.error('Erro ao buscar sorteio:', error);
+    return null;
+  }
+
+  return data;
+}

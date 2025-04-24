34965ffd617e7bf58b0c2d98ddfda5d445149826
blinbingo <ian.oddssports@gmail.com>
2025-04-24 11:47:26 -0300

    Update [codigo].js

--- pages/admin/simulador-delay/[codigo].js
+++ pages/admin/simulador-delay/[codigo].js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/router";
 import CartelasPremiadas from "../../../components/CartelasPremiadas";
 import RankingCartelas from "../../../components/RankingCartelas";

02f7ef83ddee5c029cff84aebe94ca058c78954c
blinbingo <ian.oddssports@gmail.com>
2025-04-25 16:18:03 -0300

    Update bolinhas.js

--- pages/api/bolinhas.js
+++ pages/api/bolinhas.js
@@ -53,7 +53,7 @@ export default async function handler(req, res) {
 
     if (error || !data) {
       console.log("⚠️ Nenhuma bolinha encontrada ou erro:", error);
-      return res.status(200).json({ numero: null });
+      return res.status(400).json({ numero: null });
     }
 
     // Limpa após leitura

8703a3c78bc276f629502766f0708aa6c6caf35b
blinbingo <ian.oddssports@gmail.com>
2025-04-25 16:30:22 -0300

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,10 +1,7 @@
-// /utils/supabaseClient.js
 
 import { createClient } from '@supabase/supabase-js';
 
-// Usa as variáveis de ambiente do projeto
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// Cria a instância do supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

de5e59841d47e1725cc2038bdbaf8a7a2eb013cf
blinbingo <ian.oddssports@gmail.com>
2025-04-25 16:29:50 -0300

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,10 @@
+// /utils/supabaseClient.js
+
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://drdwoahsiksqimagdnpn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyZHdvYWhzaWtzcWltYWdkbnBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5ODQ1NTIsImV4cCI6MjA2MDU2MDU1Mn0.zrrrvZRRwrwBUnFTXI_s1HBLljWyD5fqL3Rh2386a-o';
+// Usa as variáveis de ambiente do projeto
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// Cria a instância do supabase
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);

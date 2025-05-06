4ff9233a21b3dd2e8b3e1c5c4b71d6f27711114c
blinbingo <ian.oddssports@gmail.com>
2025-05-06 09:28:21 -0300

    Update index.js

--- pages/blackjack/index.js
+++ pages/blackjack/index.js
@@ -1,45 +1,51 @@
 
-import React from 'react';
+import React from "react";
 
 export default function Blackjack() {
   return (
-    <div style={
-      position: 'relative',
-      backgroundColor: '#064e3b',
-      minHeight: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }>
-      <div style={
-        position: 'relative',
-        width: '1000px',
-        height: '600px',
-        backgroundColor: '#166534',
-        borderRadius: '50% / 40%',
-        boxShadow: '0 0 20px rgba(0,0,0,0.5)'
-      }>
-        <div style={
-          position: 'absolute',
-          top: '20px',
-          left: '50%',
-          transform: 'translateX(-50%)',
-          textAlign: 'center',
-          color: '#facc15',
-          fontWeight: 'bold',
-          fontSize: '18px'
-        }>
+    <div
+      style={{
+        position: "relative",
+        backgroundColor: "#064e3b",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          width: "1000px",
+          height: "600px",
+          backgroundColor: "#166534",
+          borderRadius: "50% / 40%",
+          boxShadow: "0 0 20px rgba(0,0,0,0.5)"
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: "20px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            textAlign: "center",
+            color: "#facc15",
+            fontWeight: "bold",
+            fontSize: "18px"
+          }}
+        >
           Dealer
-          <div style={ marginTop: '10px' }>
-            <div style={
-              display: 'inline-block',
-              padding: '4px 8px',
-              backgroundColor: '#fff',
-              color: '#000',
-              borderRadius: '6px',
-              boxShadow: '0 2px 4px rgba(0,0,0,0.3)',
-              marginTop: '4px'
-            }>
+          <div style={{ marginTop: "10px" }}>
+            <div style={{
+              display: "inline-block",
+              padding: "4px 8px",
+              backgroundColor: "#fff",
+              color: "#000",
+              borderRadius: "6px",
+              boxShadow: "0 2px 4px rgba(0,0,0,0.3)",
+              marginTop: "4px"
+            }}>
               ♦ K ♠ 8
             </div>
           </div>
@@ -47,224 +53,203 @@ export default function Blackjack() {
 
         
         <div
-          style={
-            position: 'absolute',
-            left: '337.3654403270941px',
-            top: '462.63455967290594px',
-            transform: 'translate(-50%, -50%) rotate(225.0deg)',
-            color: '#bbf7d0',
-            fontWeight: 'bold',
-            textAlign: 'center',
-            fontSize: '14px',
-            width: '80px'
-          }
+          style={{
+            position: "absolute",
+            left: "337.3654403270941px",
+            top: "462.63455967290594px",
+            transform: "translate(-50%, -50%) rotate(225.0deg)",
+            color: "#bbf7d0",
+            fontWeight: "bold",
+            textAlign: "center",
+            fontSize: "14px",
+            width: "80px"
+          }}
         >
           Jogador 1
-          <div style={
-            marginTop: '10px',
-            transform: 'rotate(-225.0deg)'
-          }>
-            <div style={
-              display: 'inline-block',
-              padding: '4px 8px',
-              backgroundColor: '#fff',
-              color: '#000',
-              borderRadius: '6px',
-              boxShadow: '0 2px 4px rgba(0,0,0,0.3)',
-              marginTop: '4px'
-            }>
+          <div style={{ marginTop: "10px", transform: "rotate(-225.0deg)" }}>
+            <div style={{
+              display: "inline-block",
+              padding: "4px 8px",
+              backgroundColor: "#fff",
+              color: "#000",
+              borderRadius: "6px",
+              boxShadow: "0 2px 4px rgba(0,0,0,0.3)",
+              marginTop: "4px"
+            }}>
               ♠ A ♣ 10
             </div>
           </div>
         </div>
     
         <div
-          style={
-            position: 'absolute',
-            left: '385.00000000000006px',
-            top: '499.1858428704209px',
-            transform: 'translate(-50%, -50%) rotate(210.0deg)',
-            color: '#bbf7d0',
-            fontWeight: 'bold',
-            textAlign: 'center',
-            fontSize: '14px',
-            width: '80px'
-          }
+          style={{
+            position: "absolute",
+            left: "385.00000000000006px",
+            top: "499.1858428704209px",
+            transform: "translate(-50%, -50%) rotate(210.0deg)",
+            color: "#bbf7d0",
+            fontWeight: "bold",
+            textAlign: "center",
+            fontSize: "14px",
+            width: "80px"
+          }}
         >
           Jogador 2
-          <div style={
-            marginTop: '10px',
-            transform: 'rotate(-210.0deg)'
-          }>
-            <div style={
-              display: 'inline-block',
-              padding: '4px 8px',
-              backgroundColor: '#fff',
-              color: '#000',
-              borderRadius: '6px',
-              boxShadow: '0 2px 4px rgba(0,0,0,0.3)',
-              marginTop: '4px'
-            }>
+          <div style={{ marginTop: "10px", transform: "rotate(-210.0deg)" }}>
+            <div style={{
+              display: "inline-block",
+              padding: "4px 8px",
+              backgroundColor: "#fff",
+              color: "#000",
+              borderRadius: "6px",
+              boxShadow: "0 2px 4px rgba(0,0,0,0.3)",
+              marginTop: "4px"
+            }}>
               ♠ A ♣ 10
             </div>
           </div>
         </div>
     
         <div
-          style={
-            position: 'absolute',
-            left: '440.4716196264202px',
-            top: '522.1629400464857px',
-            transform: 'translate(-50%, -50%) rotate(195.0deg)',
-            color: '#bbf7d0',
-            fontWeight: 'bold',
-            textAlign: 'center',
-            fontSize: '14px',
-            width: '80px'
-          }
+          style={{
+            position: "absolute",
+            left: "440.4716196264202px",
+            top: "522.1629400464857px",
+            transform: "translate(-50%, -50%) rotate(195.0deg)",
+            color: "#bbf7d0",
+            fontWeight: "bold",
+            textAlign: "center",
+            fontSize: "14px",
+            width: "80px"
+          }}
         >
           Jogador 3
-          <div style={
-            marginTop: '10px',
-            transform: 'rotate(-195.0deg)'
-          }>
-            <div style={
-              display: 'inline-block',
-              padding: '4px 8px',
-              backgroundColor: '#fff',
-              color: '#000',
-              borderRadius: '6px',
-              boxShadow: '0 2px 4px rgba(0,0,0,0.3)',
-              marginTop: '4px'
-            }>
+          <div style={{ marginTop: "10px", transform: "rotate(-195.0deg)" }}>
+            <div style={{
+              display: "inline-block",
+              padding: "4px 8px",
+              backgroundColor: "#fff",
+              color: "#000",
+              borderRadius: "6px",
+              boxShadow: "0 2px 4px rgba(0,0,0,0.3)",
+              marginTop: "4px"
+            }}>
               ♠ A ♣ 10
             </div>
           </div>
         </div>
     
         <div
-          style={
-            position: 'absolute',
-            left: '500.0px',
-            top: '530.0px',
-            transform: 'translate(-50%, -50%) rotate(180.0deg)',
-            color: '#bbf7d0',
-            fontWeight: 'bold',
-            textAlign: 'center',
-            fontSize: '14px',
-            width: '80px'
-          }
+          style={{
+            position: "absolute",
+            left: "500.0px",
+            top: "530.0px",
+            transform: "translate(-50%, -50%) rotate(180.0deg)",
+            color: "#bbf7d0",
+            fontWeight: "bold",
+            textAlign: "center",
+            fontSize: "14px",
+            width: "80px"
+          }}
         >
           Jogador 4
-          <div style={
-            marginTop: '10px',
-            transform: 'rotate(-180.0deg)'
-          }>
-            <div style={
-              display: 'inline-block',
-              padding: '4px 8px',
-              backgroundColor: '#fff',
-              color: '#000',
-              borderRadius: '6px',
-              boxShadow: '0 2px 4px rgba(0,0,0,0.3)',
-              marginTop: '4px'
-            }>
+          <div style={{ marginTop: "10px", transform: "rotate(-180.0deg)" }}>
+            <div style={{
+              display: "inline-block",
+              padding: "4px 8px",
+              backgroundColor: "#fff",
+              color: "#000",
+              borderRadius: "6px",
+              boxShadow: "0 2px 4px rgba(0,0,0,0.3)",
+              marginTop: "4px"
+            }}>
               ♠ A ♣ 10
             </div>
           </div>
         </div>
     
         <div
-          style={
-            position: 'absolute',
-            left: '559.5283803735798px',
-            top: '522.1629400464857px',
-            transform: 'translate(-50%, -50%) rotate(165.0deg)',
-            color: '#bbf7d0',
-            fontWeight: 'bold',
-            textAlign: 'center',
-            fontSize: '14px',
-            width: '80px'
-          }
+          style={{
+            position: "absolute",
+            left: "559.5283803735798px",
+            top: "522.1629400464857px",
+            transform: "translate(-50%, -50%) rotate(165.0deg)",
+            color: "#bbf7d0",
+            fontWeight: "bold",
+            textAlign: "center",
+            fontSize: "14px",
+            width: "80px"
+          }}
         >
           Jogador 5
-          <div style={
-            marginTop: '10px',
-            transform: 'rotate(-165.0deg)'
-          }>
-            <div style={
-              display: 'inline-block',
-              padding: '4px 8px',
-              backgroundColor: '#fff',
-              color: '#000',
-              borderRadius: '6px',
-              boxShadow: '0 2px 4px rgba(0,0,0,0.3)',
-              marginTop: '4px'
-            }>
+          <div style={{ marginTop: "10px", transform: "rotate(-165.0deg)" }}>
+            <div style={{
+              display: "inline-block",
+              padding: "4px 8px",
+              backgroundColor: "#fff",
+              color: "#000",
+              borderRadius: "6px",
+              boxShadow: "0 2px 4px rgba(0,0,0,0.3)",
+              marginTop: "4px"
+            }}>
               ♠ A ♣ 10
             </div>
           </div>
         </div>
     
         <div
-          style={
-            position: 'absolute',
-            left: '615.0px',
-            top: '499.18584287042086px',
-            transform: 'translate(-50%, -50%) rotate(150.0deg)',
-            color: '#bbf7d0',
-            fontWeight: 'bold',
-            textAlign: 'center',
-            fontSize: '14px',
-            width: '80px'
-          }
+          style={{
+            position: "absolute",
+            left: "615.0px",
+            top: "499.18584287042086px",
+            transform: "translate(-50%, -50%) rotate(150.0deg)",
+            color: "#bbf7d0",
+            fontWeight: "bold",
+            textAlign: "center",
+            fontSize: "14px",
+            width: "80px"
+          }}
         >
           Jogador 6
-          <div style={
-            marginTop: '10px',
-            transform: 'rotate(-150.0deg)'
-          }>
-            <div style={
-              display: 'inline-block',
-              padding: '4px 8px',
-              backgroundColor: '#fff',
-              color: '#000',
-              borderRadius: '6px',
-              boxShadow: '0 2px 4px rgba(0,0,0,0.3)',
-              marginTop: '4px'
-            }>
+          <div style={{ marginTop: "10px", transform: "rotate(-150.0deg)" }}>
+            <div style={{
+              display: "inline-block",
+              padding: "4px 8px",
+              backgroundColor: "#fff",
+              color: "#000",
+              borderRadius: "6px",
+              boxShadow: "0 2px 4px rgba(0,0,0,0.3)",
+              marginTop: "4px"
+            }}>
               ♠ A ♣ 10
             </div>
           </div>
         </div>
     
         <div
-          style={
-            position: 'absolute',
-            left: '662.6345596729059px',
-            top: '462.6345596729059px',
-            transform: 'translate(-50%, -50%) rotate(135.0deg)',
-            color: '#bbf7d0',
-            fontWeight: 'bold',
-            textAlign: 'center',
-            fontSize: '14px',
-            width: '80px'
-          }
+          style={{
+            position: "absolute",
+            left: "662.6345596729059px",
+            top: "462.6345596729059px",
+            transform: "translate(-50%, -50%) rotate(135.0deg)",
+            color: "#bbf7d0",
+            fontWeight: "bold",
+            textAlign: "center",
+            fontSize: "14px",
+            width: "80px"
+          }}
         >
           Jogador 7
-          <div style={
-            marginTop: '10px',
-            transform: 'rotate(-135.0deg)'
-          }>
-            <div style={
-              display: 'inline-block',
-              padding: '4px 8px',
-              backgroundColor: '#fff',
-              color: '#000',
-              borderRadius: '6px',
-              boxShadow: '0 2px 4px rgba(0,0,0,0.3)',
-              marginTop: '4px'
-            }>
+          <div style={{ marginTop: "10px", transform: "rotate(-135.0deg)" }}>
+            <div style={{
+              display: "inline-block",
+              padding: "4px 8px",
+              backgroundColor: "#fff",
+              color: "#000",
+              borderRadius: "6px",
+              boxShadow: "0 2px 4px rgba(0,0,0,0.3)",
+              marginTop: "4px"
+            }}>
               ♠ A ♣ 10
             </div>
           </div>

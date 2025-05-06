99f0cdd5b9866c89ab80713e340944b4ad8d84be
blinbingo <ian.oddssports@gmail.com>
2025-05-06 12:17:58 -0300

    Update index.js

--- pages/blackjack/index.js
+++ pages/blackjack/index.js
@@ -37,15 +37,15 @@ export default function MesaBlackjack() {
         Dealer ♠ A ♣ 9
       </div>
 
-      {/* Jogadores - posições estimadas e rotacionadas de forma razoável */}
-      <JogadorMesa top={370} left={275} rotate={45} nome="Jogador 1" />
-      <JogadorMesa top={400} left={450} rotate={30} nome="Jogador 2" />
-      <JogadorMesa top={430} left={625} rotate={15} nome="Jogador 3" />
-      <JogadorMesa top={440} left={800} rotate={0} nome="Jogador 4" />
-      <JogadorMesa top={430} left={975} rotate={-15} nome="Jogador 5" />
-      <JogadorMesa top={400} left={1150} rotate={-30} nome="Jogador 6" />
-      <JogadorMesa top={370} left={1325} rotate={-45} nome="Jogador 7" />
-      <JogadorMesa top={325} left={1450} rotate={-60} nome="Jogador 8" />
+      {/* Jogadores com texto ajustado abaixo dos círculos */}
+      <JogadorMesa top={390} left={270} rotate={60} nome="Jogador 1" />
+      <JogadorMesa top={420} left={435} rotate={40} nome="Jogador 2" />
+      <JogadorMesa top={450} left={610} rotate={20} nome="Jogador 3" />
+      <JogadorMesa top={460} left={800} rotate={0} nome="Jogador 4" />
+      <JogadorMesa top={450} left={990} rotate={-20} nome="Jogador 5" />
+      <JogadorMesa top={420} left={1170} rotate={-40} nome="Jogador 6" />
+      <JogadorMesa top={390} left={1340} rotate={-60} nome="Jogador 7" />
+      <JogadorMesa top={350} left={1470} rotate={-80} nome="Jogador 8" />
     </div>
   );
 }

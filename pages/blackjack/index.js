22a0b5481135bd4c8f3c58c077bc13293fb6727c
blinbingo <ian.oddssports@gmail.com>
2025-05-06 13:10:01 -0300

    Update index.js

--- pages/blackjack/index.js
+++ pages/blackjack/index.js
@@ -39,14 +39,14 @@ export default function MesaBlackjack() {
       </div>
 
       {/* Jogadores com posicionamento responsivo por porcentagem */}
-      <JogadorMesa top="62%" left="14%" rotate={60} nome="Jogador 1" />
-      <JogadorMesa top="68%" left="29%" rotate={35} nome="Jogador 2" />
-      <JogadorMesa top="73%" left="44%" rotate={15} nome="Jogador 3" />
-      <JogadorMesa top="75%" left="59%" rotate={0} nome="Jogador 4" />
-      <JogadorMesa top="73%" left="74%" rotate={-15} nome="Jogador 5" />
-      <JogadorMesa top="68%" left="89%" rotate={-35} nome="Jogador 6" />
-      <JogadorMesa top="62%" left="97%" rotate={-60} nome="Jogador 7" />
-      <JogadorMesa top="55%" left="99%" rotate={-80} nome="Jogador 8" />
+      <JogadorMesa top="75%" left="15%" rotate={60} nome="Jogador 1" />
+      <JogadorMesa top="75%" left="25%" rotate={35} nome="Jogador 2" />
+      <JogadorMesa top="75%" left="35%" rotate={15} nome="Jogador 3" />
+      <JogadorMesa top="75%" left="45%" rotate={0} nome="Jogador 4" />
+      <JogadorMesa top="75%" left="55%" rotate={-15} nome="Jogador 5" />
+      <JogadorMesa top="75%" left="65%" rotate={-35} nome="Jogador 6" />
+      <JogadorMesa top="75%" left="75%" rotate={-60} nome="Jogador 7" />
+      <JogadorMesa top="75%" left="85%" rotate={-80} nome="Jogador 8" />
     </div>
   );
 }
